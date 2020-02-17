db0df3d2dfc038eb0bf914d38e67bc2283c4831d
sanaaDaire <sanaa.daire@icloud.com>
2020-02-17 16:24:51 +0200

    welcome!

---
+++ js/app.js
@@ -0,0 +1,70 @@
+'use strict'; 
+
+var know = prompt(' Hey! Do you know me ? ').toLowerCase();
+
+if (know === 'yes') {
+    alert(' Amazing! welcome in my site ;) ');
+}
+else if (know === 'no') {
+    alert(' I am sanaa , WELCOME!');
+}
+else {
+    alert ('Oops! you dont answer , please answer next question');
+}
+console.log(know);
+
+var name = prompt('  Do you know whats my full name?');
+if (name === 'yes') {
+    alert(' GOOD! ');
+}
+else if (name === 'no') {
+    alert(' I am SANAA DAIRE , WELCOME!')
+}
+else { alert ('Oops! you dont answer , please answer next question'); }
+console.log(name);
+
+
+var age = prompt(' Do you know im 23 years ?');
+if (age === 'yes') {
+    alert(' So happy to know my age darling ');
+}
+else if (age === 'no') {
+    alert(' happy to say im 23 years old for you');
+}
+else { alert ('Oops! you dont answer , please answer next question'); }
+
+console.log(age);
+
+
+var study = prompt('Do you know what im study?');
+if (study === 'yes') {
+    alert(' well! ');
+}
+else if (study === 'no') {
+    alert(' well! Software Engineering ')
+}
+else {alert ('Oops! you dont answer , please answer next question'); }
+
+console.log(study);
+
+
+var food = prompt('Do you know whats my favourite food?');
+if (food === 'yes') {
+    alert(' AMAZING! ');
+}
+else if (food === 'no') {
+    alert(' HUMMM! i am in love with Burger  ')
+}
+else {alert ('Oops! you dont answer , please answer next question'); }
+
+console.log(food);
+
+alert(" WELCOME in my personal site Dear , i wish U interest here!");
+
+var name1 = prompt(" let me know , Whats your name ?")
+alert(" Greet Welcome  " + name1);
+
+alert(" Welcome again " + name1);
+
+
+
